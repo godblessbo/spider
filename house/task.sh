369875af06e8cf7d4b572a2eed81a1f5f9e54811
tianfei <wangyitianfei@163.com>
2016-06-02 00:57:19 +0800

    【添加定时任务】

---
+++ house/task.sh
@@ -0,0 +1,7 @@
+#! /bin/sh                                                                                                                                            
+
+export PATH=$PATH:/usr/local/bin
+
+cd /spider/house/
+
+scrapy crawl house
